e096d7f30fda4498ab041922a2b4ccc81d61da60
canvapr01070 <canvapr01070@gmail.com>
2026-01-13 23:50:20 +0100

    first commit

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+router.get("/me", (req, res) => {
+  if (!req.session.user) return res.status(401).json({ error: "Not logged in" });
+  res.json(req.session.user);
+});
+
+module.exports = router;
