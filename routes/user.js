daba11d28877fda6df4d025eaad9fd5b6df764c3
canvapr01070-netizen <canvapr01070@gmail.com>
2026-01-14 03:11:21 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 router.get("/me", (req, res) => {
-  if (!req.session.user) return res.status(401).json({ error: "Not logged in" });
+  if (!req.session.user) {
+    return res.status(401).json({ error: "Not logged in" });
+  }
   res.json(req.session.user);
 });
 
